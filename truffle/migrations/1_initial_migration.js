951347bc24a1f472ce43372f22152ac18c000b2d
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2024-04-11 15:47:28 +0900

    feat: migrations 파일에 helloworld compile json 추가

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -3,3 +3,10 @@ const Migrations = artifacts.require("Migrations");
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
 };
+
+// build 디렉토리 안의 HelloWorld json 파일 가져오기
+const helloworld = artifacts.require('HelloWorld'); 
+ 
+module.exports = function (deployer) {
+    deployer.deploy(helloworld);
+};
